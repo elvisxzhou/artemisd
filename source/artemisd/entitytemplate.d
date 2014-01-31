c62b5e69569dc30322e144bb14c3ac5512828a4b
Ryan Scott <atthealma@gmail.com>
2014-01-31 22:49:37 +1100

    Added typeid to entity templates.

--- source/artemisd/entitytemplate.d
+++ source/artemisd/entitytemplate.d
@@ -2,7 +2,11 @@ module artemisd.entitytemplate;
 
 import artemisd.entity;
 import artemisd.world;
+import artemisd.utils.type;
 
-abstract class EntityTemplate {
+abstract class EntityTemplate 
+{
+  mixin TypeDecl;
+
   static Entity buildEntity(Args...)(Entity e, World world, Args args);
 }
